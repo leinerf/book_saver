00581d55998d91a53d54f1ae03b3f180dc34675c
Freniel Zabala <frenielzabala@macbookpro.attlocal.net>
2026-03-29 20:47:48 -0700

    added books route

--- routes/views/index.js
+++ routes/views/index.js
@@ -4,7 +4,10 @@ import axios from "axios"
 export default function viewRoutes() {
     const router = express.Router();
     const api = axios.create({ baseURL: process.env.BASE_API || "http://localhost:3000/api" })
-    router.get("/books", (req, res) => {;
+    router.get("/books", async(req, res) => {
+        const response = await api.get("/books")
+        console.log(response.data)
+        return res.render("books.ejs")
     })
     router.get(
         "/users/:uid/reviews",
